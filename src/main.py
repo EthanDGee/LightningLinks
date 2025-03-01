3ef8dcd3d1e2435b43faa8b77e5b40c147295277
EthanDGee <workethangee@gmail.com>
2025-02-28 20:58:54 -0700

    Added extract sentences function; Got model loaded, and running locally; Added print statements for showing stages;

--- src/main.py
+++ src/main.py
@@ -2,32 +2,47 @@ import note_handler
 from sentence_transformers import SentenceTransformer
 
 
-def find_similar_sentences(model, sentences):
-    embedding = model.encode(sentences)
+def find_similarities(model, sentences):
+    embedding = model.encode(sentences, max_length=256, show_progress_bar=True)
 
     similarities = model.similarity(embedding, embedding)
     return similarities
 
+
 def extract_sentences(notes_list):
+    notes_sentences = []
 
+    for individual_note in notes_list:
+        notes_sentences.append(individual_note["body"])
 
-
+    return notes_sentences
 
 
 if __name__ == "__main__":
+
+    print("Loading Files...", end="")
     # get files
     note_directory = "../demoData/"
-    notes = note_handler.get_all_markdown_files(note_directory)
+    notes = note_handler.load_all_markdown_files(note_directory)
+    print("\rFiles Loaded!")
+
     # get sentences
-
-
+    print("Extracting Sentences...", end="")
+    sentences = extract_sentences(notes)
+    print("\rSentences Extracted!")
 
     # load model
     # source https://huggingface.co/BAAI/bge-m3
-    model = SentenceTransformer('BAAI/bge-m3')
+    print("Loading Model...",)
+    model = SentenceTransformer('sentence-transformers/all-mpnet-base-v2')
+    print("Model Loaded!")
 
     # encode sentences
+    print("Encoding Sentences...", end="")
+    encoded_sentences = find_similarities(model, sentences)
 
+    print(encoded_sentences)
+    print("\rSentences Encoded!")
     # rerank
 
     # append to file ends
